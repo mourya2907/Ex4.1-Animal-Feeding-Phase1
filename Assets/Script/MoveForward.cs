7650ea63389598a927ac98214b9135a6f4d926d1
mourya2907 <mouryamoo2907@gmail.com>
2025-05-31 15:01:37 +0530

    Update MoveForward.cs

--- Assets/Script/MoveForward.cs
+++ Assets/Script/MoveForward.cs
@@ -1,20 +1,20 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    public float speed=20.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
     }
-
 }
